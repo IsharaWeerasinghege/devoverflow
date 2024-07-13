d6b26f62517d0d268e42a08e46ac80d6f61484ab
W Suranga ishara <92960498+IsharaWeerasinghege@users.noreply.github.com>
2024-07-13 16:34:00 +0530

    doing authentication with clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-center text-4xl font-bold"> Hello World! </h1>
+      hello world
+      <UserButton afterSignOutUrl={"/"} />
     </main>
   );
 }
